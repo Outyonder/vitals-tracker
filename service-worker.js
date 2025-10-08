45fa0678ad6138bbf579ca41a3be99f123c2e961
Outyonder <Techslave57@gmail.com>
2025-10-07 21:52:11 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME='vitals-cache-v1';
+const CACHE_NAME='vitals-cache-v2';
 const PRECACHE=['./','./index.html','./styles.css','./main.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(PRECACHE))); self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
